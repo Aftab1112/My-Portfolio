f5992fdc7fa1f451ff1813b526baba4f8032303a
Aftab1112 <you@example.com>
2024-03-05 01:34:48 +0530

    completed with navbar and made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-center">Hello</h1>
-    </>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export default function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "projects",
+    },
+    {
+      id: 4,
+      link: "technologies",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white fixed bg-black px-4">
+      <div>
+        <h1 className="text-5xl font-signature ml-2 cursor-pointer relative z-10 ">
+          Aftab
+        </h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize text-gray-500 hover:text-white hover:shadow-lg hover:bg-white/30 hover:rounded-full duration-500 hover:scale-105 hover:ring-2 hover:ring-blue-500 hover:ring-opacity-95 text-xl"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul
+          className="transition-top duration-500 flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500"
+          style={{ top: "80px" }}
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-5 text-4xl  hover:text-white hover:shadow-lg hover:bg-white/30 hover:rounded-full duration-500 hover:scale-75 hover:ring-2 hover:ring-blue-500 hover:ring-opacity-95"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
