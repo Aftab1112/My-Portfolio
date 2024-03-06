0693c1401dbccade08b0e55482efd6df9b6c3b15
Aftab1112 <you@example.com>
2024-03-06 19:16:47 +0530

    added react to app.jsx :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
